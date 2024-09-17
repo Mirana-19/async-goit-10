02017169fa60896dc8b78b79674c4e049ad5c3f1
Marta Żak <martysik19@gmail.com>
2024-09-18 01:13:43 +0200

    adds timer

--- main.js
+++ main.js
@@ -1,24 +1,87 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let userSelectedDate;
+let intervalId = null;
+const startButton = document.querySelector('[data-start]');
+const inputDateTime = document.querySelector('#datetime-picker');
+const dateFields = document.querySelectorAll('.value');
+startButton.disabled = true;
 
-setupCounter(document.querySelector('#counter'))
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+  },
+  onChange(selectedDates) {
+    userSelectedDate = selectedDates[0];
+
+    if (!isDateInTheFuture(userSelectedDate)) {
+      alert('Select a valid date in the future!');
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  },
+};
+
+startButton.addEventListener('click', startCountdown);
+
+function startCountdown() {
+  startButton.disabled = true;
+  intervalId = setInterval(() => {
+    const timeRemaining = userSelectedDate.getTime() - new Date();
+
+    if (!isDateInTheFuture(userSelectedDate)) {
+      startButton.disabled = false;
+      return clearInterval(intervalId);
+    }
+    updateTimer(convertMs(timeRemaining));
+  }, 1000);
+}
+
+function isDateInTheFuture(date) {
+  return date.getTime() > new Date().getTime();
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return {
+    days: formatTime(days),
+    hours: formatTime(hours),
+    minutes: formatTime(minutes),
+    seconds: formatTime(seconds),
+  };
+}
+
+function formatTime(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateTimer(time) {
+  for (const field of dateFields) {
+    const key = Object.keys(field.dataset)[0];
+
+    field.textContent = time[key];
+  }
+}
+
+flatpickr(inputDateTime, options);
